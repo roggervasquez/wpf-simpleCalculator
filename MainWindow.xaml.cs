fb78efd62debb41fd57923a4645481b098405074
Rogger <rogger.vasquez@gmail.com>
2020-08-15 18:42:01 -0600

    Adding basic functionality to AC, Negative and percentage button using event handlers, and a generic handler for numbers

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,12 +21,51 @@ namespace wpf_simpleCalculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        double lastNumber, result;
+
         public MainWindow()
         {
             InitializeComponent();
-           
+            acButton.Click += AcButton_Click;
+            signButton.Click += SignButton_Click;
+            percentageButton.Click += PercentageButton_Click;
         }
 
-      
+        private void PercentageButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(resultLabel.Content.ToString(), out lastNumber))
+            {
+                lastNumber = lastNumber / 100;
+                resultLabel.Content = lastNumber.ToString();
+            }
+        }
+
+        private void SignButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(resultLabel.Content.ToString(), out lastNumber))
+            {
+                lastNumber = lastNumber * -1;
+                resultLabel.Content = lastNumber.ToString();
+            }
+        }
+
+        private void AcButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            this.resultLabel.Content = "0";
+        }
+
+        private void Number_Click(object sender, RoutedEventArgs e)
+        {
+            string  content = (sender as Button).Content.ToString();
+            if(resultLabel.Content.ToString() == "0")
+            {
+                resultLabel.Content = content;
+            }
+            else
+            {
+                resultLabel.Content = $"{resultLabel.Content}{content}";
+            }
+        }
     }
 }
